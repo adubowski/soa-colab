604677b75923043c84eb222fca60af5451a8fd98
Adam <adubowski98@gmail.com>
2021-03-21 20:38:24 +0100

    fix missing rest controller annotation

--- meeting-handler/src/main/java/nl/utwente/soa/meetinghandler/app/rest/MeetingRestController.java
+++ meeting-handler/src/main/java/nl/utwente/soa/meetinghandler/app/rest/MeetingRestController.java
@@ -5,8 +5,13 @@ import nl.utwente.soa.meetinghandler.app.services.MeetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
+@RestController
+@RequestMapping(path = "api/v1/meetings")
 public class MeetingRestController {
 
     private final MeetingService meetingService;
